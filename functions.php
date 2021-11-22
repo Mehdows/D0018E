c6ce0856aac702d4025c40ffc91a8afceacd4cda
TovahParnes <tovah@parnes.com>
2021-11-22 14:01:15 +0100

    added functions: connect, closeconnection, getItems. Display items on homepage

---
+++ functions.php
@@ -0,0 +1,34 @@
+<?php
+
+
+function startConnection() {
+  $servername = "130.240.200.63";
+  $username = "ConnectUser";
+  $password = "ConnectUser";
+  $db = "D0018E";
+
+  // Create connection
+  $conn = mysqli_connect($servername, $username, $password, $db);
+
+  // Check connection
+  if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
+  echo "Connected successfully";
+  return $conn;
+}
+
+
+function closeConnection($conn) {
+  $conn->close();
+  echo "Closed connection";
+}
+
+
+function getAllItems($conn) {
+    $sql = "SELECT item_ID, name, price, image FROM Items";
+    $result = $conn->query($sql);
+    
+    return $result;
+}
+?>
---
+++ homePage.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+<title>RS-MQF 1200 V.2</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="styles/layout.css" type="text/css" media="all">
+<link rel="stylesheet" href="styles/mediaqueries.css" type="text/css" media="all">
+<link rel="stylesheet" href="styles/button.css" type="text/css" media="all">
+<script src="scripts/jquery.1.9.0.min.js"></script>
+<script src="scripts/jquery-mobilemenu.min.js"></script>
+<!--[if lt IE 9]>
+<link rel="stylesheet" href="styles/ie.css" type="text/css" media="all">
+<script src="scripts/ie/css3-mediaqueries.min.js"></script>
+<script src="scripts/ie/ie9.js"></script>
+<script src="scripts/ie/html5shiv.min.js"></script>
+<![endif]-->
+<!-- BEFORE USING THIS FRAMEWORK REMOVE THIS DEMO STYLING - ONLY USED TO EMPHASISE THE DIV CONTAINERS IN THE CONTENT AREA -->
+<style type="text/css">
+div.full_width{margin-top:20px;}
+div.full_width:first-child{margin-top:0;}
+div.full_width div{color:#666666; background-color:#fffefe;}
+</style>
+<!-- END DEMO STYLING -->
+</head>
+<body>
+  <?php
+		require __DIR__ . '/functions.php';
+		$conn = startConnection();
+  ?>
+<div class="wrapper row1">
+  <header id="header" class="clear">
+    <div id="hgroup">
+      <h1><a href="#">Bananazon</a></h1>
+      <h2>Our banana store</h2>
+    </div>
+  </header>
+</div>
+<!-- ################################################################################################ -->
+<div class="wrapper row2">
+  <nav id="topnav">
+    <ul class="clear">
+      <li class="active first"><a href="homePage.html">Homepage</a></li>
+      <li><a href="#"></a></li>
+      <li><a href="orderHistory.html">Order history</a></li>
+      <li><a href="shoppingCart.html">Cart</a></li>
+      <li><a href="login.html">Logout</a></li>
+    </ul>
+  </nav>
+</div>
+<!-- content -->
+<div class="wrapper row3">
+<div id="container">
+<!-- ################################################################################################ -->
+<div class="full_width clear">
+
+  <?php
+    $result = getAllItems($conn);
+
+    echo('<table>');
+      echo('<tr>');
+      while ($row = mysqli_fetch_assoc($result)) {
+        if((htmlentities($row['item_ID']))%3 == 1){ //only display 3 items per row
+          echo('</tr>');
+          echo('<tr>');
+        }
+        echo('<td>');
+        echo('<div class="imgContainer">');
+          echo('<div>');
+                    echo('<div>');
+              echo("<h2>".htmlentities($row['name']). " - " . htmlentities($row['price']). " kr/st</h2>");
+              echo('<a href="inspect.html" ><img src='.htmlentities($row['image']).' style="width:300px;height:300px;"></a>');
+            echo('</div>');
+                    echo('<div class="imgButton">');
+              echo('<a class="button" href="/inspect.html">Buy</a>');
+            echo('</div>');
+          echo('</div>');
+        echo('</div>');
+        echo('</td>');
+      }
+      echo('</tr>');
+    echo('</table>');
+  ?>
+
+<?php
+  closeConnection($conn);
+?>
+</body>
+</html>
